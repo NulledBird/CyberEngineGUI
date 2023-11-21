c57fe92a0c5447661120905bc01e74a97065eabf
Karl Arabit <sola30526@gmail.com>
2023-11-20 20:13:03 -0500

    Commit?
    this took so long please work

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { appWindow } from '@tauri-apps/api/window'
+document
+  .getElementById('titlebar-minimize')
+  .addEventListener('click', () => appWindow.minimize())
+document
+  .getElementById('titlebar-maximize')
+  .addEventListener('click', () => appWindow.toggleMaximize())
+document
+  .getElementById('titlebar-close')
+  .addEventListener('click', () => appWindow.close())
+
+
+const { invoke } = window.__TAURI__.tauri;
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("hello");
+});
